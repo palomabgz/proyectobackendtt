3d32ca4446022993baefc796247f959c98085b71
Paloma González <palomabgz@gmail.com>
2025-06-25 15:52:15 -0300

    actualización db

--- express/src/config/db.js
+++ express/src/config/db.js
@@ -1,21 +1,26 @@
 const users = [
     {
+        id: 1,
         name: 'Paloma Belén González',
         email: 'paloma@gmail.com'
     },
     {
+        id: 2,
         name: 'Esteban Gomez',
         email: 'esteban@gmail.com'
     },
     {
+        id: 3,
         name: 'Nicolas Sánches',
         email: 'nicolas@gmail.com'
     },
     {
+        id: 4,
         name: 'Matias Paccot',
         email: 'matias@gmail.com'
     },
     {
+        id: 5,
         name: 'Lorena Martinez',
         email: 'matias@gmail.com'
     }
