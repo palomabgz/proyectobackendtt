c5ecd65d06acb1507328e552a7ea82453b6ffbae
Paloma González <palomabgz@gmail.com>
2025-07-07 16:21:21 -0300

    actualización cors

--- express-tt2025/src/index.js
+++ express-tt2025/src/index.js
@@ -5,16 +5,14 @@ import productRoutes from './routes/product.routes.js';
 const PORT  = 5000;
 const app = express();
 
-//middlewares
-app.use(express.json());
-app.use(cors());
-
 const corsOptions = {
     origin: 'http://127.0.0.1:5501',
     methods: ['GET','HEAD','PUT','POST','DELETE'],
 };
 
-app.use(cors(corsOptions));
+//middlewares
+app.use(express.json());
+app.use(cors());
 
 //routes
 app.use('/api/products', productRoutes);
